031fb5718da1bfc296d6cd8953b49f06764f2f49
Merlin Beutlberger <m.beutlberger@sap.com>
2025-12-10 15:24:00 +0100

    refactor(builder): Rename task param 'buildCache' to 'cacheUtil'

--- packages/builder/lib/tasks/replaceVersion.js
+++ packages/builder/lib/tasks/replaceVersion.js
@@ -14,16 +14,17 @@ import stringReplacer from "../processors/stringReplacer.js";
  *
  * @param {object} parameters Parameters
  * @param {@ui5/fs/DuplexCollection} parameters.workspace DuplexCollection to read and write files
+ * @param {object} parameters.cacheUtil Cache utility instance
  * @param {object} parameters.options Options
  * @param {string} parameters.options.pattern Pattern to locate the files to be processed
  * @param {string} parameters.options.version Replacement version
  * @returns {Promise<undefined>} Promise resolving with <code>undefined</code> once data has been written
  */
-export default async function({workspace, buildCache, options: {pattern, version}}) {
+export default async function({workspace, cacheUtil, options: {pattern, version}}) {
 	let resources = await workspace.byGlob(pattern);
 
-	if (buildCache.hasCache()) {
-		const changedPaths = buildCache.getChangedProjectResourcePaths();
+	if (cacheUtil.hasCache()) {
+		const changedPaths = cacheUtil.getChangedProjectResourcePaths();
 		resources = resources.filter((resource) => changedPaths.has(resource.getPath()));
 	}
 	const processedResources = await stringReplacer({
