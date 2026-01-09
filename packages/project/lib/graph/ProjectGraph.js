b5b06324abbe09e1db95bd71939810e6cbf7bcf8
Matthias Osswald <mat.osswald@sap.com>
2026-01-09 15:00:48 +0100

    refactor(project): Remove unused 'cacheDir' param

--- packages/project/lib/graph/ProjectGraph.js
+++ packages/project/lib/graph/ProjectGraph.js
@@ -632,7 +632,6 @@ class ProjectGraph {
 	 * @param {Array.<string>} [parameters.excludedTasks=[]] List of tasks to be excluded.
 	 * @param {module:@ui5/project/build/ProjectBuilderOutputStyle} [parameters.outputStyle=Default]
 	 *   Processes build results into a specific directory structure.
-	 * @param {string} [parameters.cacheDir] Path to the cache directory
 	 * @param {boolean} [parameters.watch] Whether to watch for file changes and re-execute the build automatically
 	 * @returns {Promise} Promise resolving to <code>undefined</code> once build has finished
 	 */
@@ -643,7 +642,7 @@ class ProjectGraph {
 		selfContained = false, cssVariables = false, jsdoc = false, createBuildManifest = false,
 		includedTasks = [], excludedTasks = [],
 		outputStyle = OutputStyleEnum.Default,
-		cacheDir, watch,
+		watch,
 	}) {
 		this.seal(); // Do not allow further changes to the graph
 		if (this._built) {
@@ -668,7 +667,6 @@ class ProjectGraph {
 			destPath, cleanDest,
 			includedDependencies, excludedDependencies,
 			dependencyIncludes,
-			// cacheDir, // FIXME/TODO: Not implemented yet
 			watch,
 		});
 	}
