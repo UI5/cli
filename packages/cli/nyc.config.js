d2f874b76a710c39005a7f359e5f79105808bfff
Matthias Osswald <mat.osswald@sap.com>
2026-01-12 13:45:53 +0100

    test: Move nyc config to file
    Using a configuration file allows for a better reuse of common settings across multiple packages.
    Coverage thresholds have been increased for several packages to ensure a high code quality.

---
+++ packages/cli/nyc.config.js
@@ -0,0 +1,9 @@
+import nycCommonConfig from "../../nyc.common.config.js";
+
+export default {
+	...nycCommonConfig,
+	"statements": 95,
+	"branches": 90,
+	"functions": 85,
+	"lines": 95,
+};
---
+++ packages/fs/nyc.config.js
@@ -0,0 +1,9 @@
+import nycCommonConfig from "../../nyc.common.config.js";
+
+export default {
+	...nycCommonConfig,
+	"statements": 90,
+	"branches": 85,
+	"functions": 90,
+	"lines": 90,
+};
