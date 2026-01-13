faad1a818b99ab6e15e5e64d10f0b25b14349489
Matthias Osswald <mat.osswald@sap.com>
2026-01-13 14:32:36 +0100

    test(project): Add failing ProjectBuilder test case

--- packages/project/test/lib/build/ProjectBuilder.integration.js
+++ packages/project/test/lib/build/ProjectBuilder.integration.js
@@ -1,10 +1,10 @@
 import test from "ava";
 import sinonGlobal from "sinon";
 import {fileURLToPath} from "node:url";
-import ProjectGraph from "../../../lib/graph/ProjectGraph.js";
+import fs from "node:fs/promises";
 import ProjectBuilder from "../../../lib/build/ProjectBuilder.js";
-import Application from "../../../lib/specifications/types/Application.js";
 import * as taskRepository from "@ui5/builder/internal/taskRepository";
+import {graphFromPackageDependencies} from "../../../lib/graph/graph.js";
 
 test.beforeEach((t) => {
 	t.context.sinon = sinonGlobal.createSandbox();
@@ -15,64 +15,49 @@ test.afterEach.always((t) => {
 	delete process.env.UI5_DATA_DIR;
 });
 
-test.serial("Build application project twice without changes", async (t) => {
-	const {sinon} = t.context;
+test.serial("Build application project multiple times", async (t) => {
+	const fixtureTester = new FixtureTester(t, "application.a");
 
-	process.env.UI5_DATA_DIR = getTmpPath("application.a/.ui5");
+	let projectBuilder;
+	const destPath = fixtureTester.destPath;
 
-	async function createProjectBuilder() {
-		const customApplicationProject = new Application();
-		await customApplicationProject.init({
-			"id": "application.a",
-			"version": "1.0.0",
-			"modulePath": getFixturePath("application.a"),
-			"configuration": {
-				"specVersion": "5.0",
-				"type": "application",
-				"metadata": {
-					"name": "application.a"
-				},
-				"kind": "project"
-			}
-		});
-
-		const graph = new ProjectGraph({
-			rootProjectName: customApplicationProject.getName(),
-		});
-		graph.addProject(customApplicationProject);
-		graph.seal(); // Graph needs to be sealed before building
-
-		const buildConfig = {};
-
-		const projectBuilder = new ProjectBuilder({
-			graph,
-			taskRepository,
-			buildConfig
-		});
-		sinon.spy(projectBuilder, "_buildProject");
-		return projectBuilder;
-	}
-
-	const destPath = getTmpPath("application.a/dist");
-
-	let projectBuilder = await createProjectBuilder();
-
-	// First build (with empty cache)
+	// #1 build (with empty cache)
+	projectBuilder = await fixtureTester.createProjectBuilder();
 	await projectBuilder.build({destPath});
 
 	t.is(projectBuilder._buildProject.callCount, 1);
 	t.is(
 		projectBuilder._buildProject.getCall(0).args[0].getProject().getName(),
 		"application.a",
-		"application.a built in first build"
+		"application.a built in build #1"
 	);
 
-	// Second build (with cache, no changes)
-	projectBuilder = await createProjectBuilder();
-
+	// #2 build (with cache, no changes)
+	projectBuilder = await fixtureTester.createProjectBuilder();
 	await projectBuilder.build({destPath});
 
-	t.is(projectBuilder._buildProject.callCount, 0, "No projects built in second build");
+	t.is(projectBuilder._buildProject.callCount, 0, "No projects built in build #2");
+
+	// Change a source file in application.a
+	const changedFilePath = `${fixtureTester.fixturePath}/webapp/test.js`;
+	await fs.appendFile(changedFilePath, `\ntest("line added");\n`);
+
+	// #3 build (with cache, with changes)
+	projectBuilder = await fixtureTester.createProjectBuilder();
+	await projectBuilder.build({destPath});
+
+	t.is(projectBuilder._buildProject.callCount, 1);
+	t.is(
+		projectBuilder._buildProject.getCall(0).args[0].getProject().getName(),
+		"application.a",
+		"application.a rebuilt in build #3"
+	);
+
+	// #4 build (with cache, no changes)
+	projectBuilder = await fixtureTester.createProjectBuilder();
+	await projectBuilder.build({destPath});
+
+	t.is(projectBuilder._buildProject.callCount, 0, "No projects built in build #4");
 });
 
 function getFixturePath(fixtureName) {
@@ -81,4 +66,40 @@ function getFixturePath(fixtureName) {
 
 function getTmpPath(folderName) {
 	return fileURLToPath(new URL(`../../tmp/${folderName}`, import.meta.url));
+}
+
+class FixtureTester {
+	constructor(t, fixtureName) {
+		this._sinon = t.context.sinon;
+		this._fixtureName = fixtureName;
+		this._initialized = false;
+
+		// Public
+		this.fixturePath = getTmpPath(fixtureName);
+		this.destPath = getTmpPath(`${fixtureName}/dist`);
+	}
+
+	async _initialize() {
+		if (this._initialized) {
+			return;
+		}
+		process.env.UI5_DATA_DIR = getTmpPath(`${this._fixtureName}/.ui5`);
+		await fs.cp(getFixturePath(this._fixtureName), this.fixturePath, {recursive: true});
+		this._initialized = true;
+	}
+
+	async createProjectBuilder() {
+		await this._initialize();
+		const graph = await graphFromPackageDependencies({
+			cwd: this.fixturePath
+		});
+		graph.seal();
+		const projectBuilder = new ProjectBuilder({
+			graph,
+			taskRepository,
+			buildConfig: {}
+		});
+		this._sinon.spy(projectBuilder, "_buildProject");
+		return projectBuilder;
+	}
 }
