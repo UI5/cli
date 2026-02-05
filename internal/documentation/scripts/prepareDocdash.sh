bb9a7d36647cad0a5d931f0552199fb265568755
Gianluca Beil <g.beil@sap.com>
2026-02-05 08:15:34 +0100

    docs: Remove docdash folder completely

--- internal/documentation/scripts/prepareDocdash.sh
+++ internal/documentation/scripts/prepareDocdash.sh
@@ -30,25 +30,6 @@ echo ""
 echo "Copying modified templates to docdash"
 cp -Rv "jsdoc/patches/tmpl" "$DOCDASH_DESTINATION/tmpl"
 cp -v "$DOCDASH_PATH/publish.js" "$DOCDASH_DESTINATION/"
-cat > jsdoc/docdash/README.md << 'EOF'
-This folder includes files from [docdash 2.0.2](https://github.com/clenemt/docdash/tree/bee5d0789068be6a1e01ce02968b23dd021b4fb6), which is a documentation template for JSDoc.
-
-These files have been modified to change the output from HTML to Markdown, and to remove unnecessary features for our use case.
-
-## Modifications by SAP
-* Output Markdown instead of HTML (Modified all templates)
-* Removed navbar functionality from publish.js
-* Removed static files functionality from publish.js
-* Modified js links to link to GitHub
-* Modified internal linking
-
-## Contributors
-
-Thanks to [lodash](https://lodash.com) and [minami](https://github.com/nijikokun/minami).
-
-## License
-Licensed under the Apache License, version 2.0. (see [Apache-2.0](LICENSE.md)).
-EOF
 cat > jsdoc/docdash/package.json << 'EOF'
 {
 	"type": "commonjs"
