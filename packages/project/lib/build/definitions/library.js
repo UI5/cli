e3de6c6c3dbbfcd3b9b6d04263a05dd4f4d22803
Merlin Beutlberger <m.beutlberger@sap.com>
2026-02-10 11:13:03 +0100

    refactor(project): Rename task param 'supportsDifferentialUpdates' => 'supportsDifferentialBuilds'
    For better consistency

--- packages/project/lib/build/definitions/library.js
+++ packages/project/lib/build/definitions/library.js
@@ -20,7 +20,7 @@ export default function({project, taskUtil, getTask}) {
 	});
 
 	tasks.set("replaceCopyright", {
-		supportsDifferentialUpdates: true,
+		supportsDifferentialBuilds: true,
 		options: {
 			copyright: project.getCopyright(),
 			pattern: "/**/*.{js,library,css,less,theme,html}"
@@ -28,7 +28,7 @@ export default function({project, taskUtil, getTask}) {
 	});
 
 	tasks.set("replaceVersion", {
-		supportsDifferentialUpdates: true,
+		supportsDifferentialBuilds: true,
 		options: {
 			version: project.getVersion(),
 			pattern: "/**/*.{js,json,library,css,less,theme,html}"
@@ -36,7 +36,7 @@ export default function({project, taskUtil, getTask}) {
 	});
 
 	tasks.set("replaceBuildtime", {
-		supportsDifferentialUpdates: true,
+		supportsDifferentialBuilds: true,
 		options: {
 			pattern: "/resources/sap/ui/{Global,core/Core}.js"
 		}
@@ -85,7 +85,7 @@ export default function({project, taskUtil, getTask}) {
 	}
 
 	tasks.set("minify", {
-		supportsDifferentialUpdates: true,
+		supportsDifferentialBuilds: true,
 		options: {
 			pattern: minificationPattern
 		}
