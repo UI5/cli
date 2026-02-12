cb749a3bc9d9bfa3f584249857e491eb970df597
OpenUI5 <openui5@sap.com>
2026-02-12 13:10:45 +0000

    Updating supplemental resources for v5 documentation deployment

---
+++ next/assets/pages_FAQ.md.B1703wMF.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,o as r,c as t,ag as n}from"./chunks/framework.D2nl_RNf.js";const u=JSON.parse('{"title":"FAQ","description":"","frontmatter":{},"headers":[],"relativePath":"pages/FAQ.md","filePath":"pages/FAQ.md"}'),o={name:"pages/FAQ.md"};function s(i,e,p,h,c,l){return r(),t("div",null,[...e[0]||(e[0]=[n("",16)])])}const f=a(o,[["render",s]]);export{u as __pageData,f as default};
