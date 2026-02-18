bdae0d639381b6b8c64088bcd38e4a329543a491
Max Reichmann <max.reichmann@sap.com>
2026-02-18 16:02:41 +0100

    test(project): Fix multiple-task tests (Address review)
    `+` Fix logic for preceding tasks being able to read tags
    `+` Clean-up custom-task code

--- packages/project/test/fixtures/application.a/custom-tasks/custom-task-0.js
+++ packages/project/test/fixtures/application.a/custom-tasks/custom-task-0.js
@@ -4,26 +4,16 @@ module.exports = async function ({
 }) {
 	console.log("Custom task 0 executed");
 
-	// For #1 Build: Read a file which is an input of custom-task-1 (which sets a tag on it)
+	// Read a file which is an input of custom-task-1 (which sets a tag on it):
 	const testJS = await workspace.byPath(`/resources/${projectNamespace}/test.js`);
 
-	// For #3 Build: Read a different file (which is NOT an input of custom-task-1)
-	const test2JS = await workspace.byPath(`/resources/${projectNamespace}/test2.js`);
-
 	const tag = taskUtil.getTag(testJS, taskUtil.STANDARD_TAGS.IsDebugVariant);
-	if (!test2JS && testJS) {
-		// For #1 Build:
-		if (tag) {
-			throw new Error("Tag set by custom-task-1 is present in custom-task-0, which is UNEXPECTED.");
-		} else {
-			console.log("Tag set by custom-task-1 is not present in custom-task-0, as EXPECTED.");
-		}
-	} else {
-		// For #3 Build (NEW behavior expected as in #1):
-		if (tag) {
-			console.log("Tag set by custom-task-1 is present in custom-task-0 now, as EXPECTED.");
-		} else {
-			throw new Error("Tag set by custom-task-1 is NOT present in custom-task-0, which is UNEXPECTED.");
-		}
+	// For #1 & #3 build:
+	if (tag) {
+		throw new Error("Tag set by custom-task-1 is present in custom-task-0, which is UNEXPECTED.");
 	}
+
+	// For #3 build: Read a different file which is not an input of custom-task-1
+	// (ensures that this task is executed):
+	const test2JS = await workspace.byPath(`/resources/${projectNamespace}/test2.js`);
 };
--- packages/project/test/fixtures/application.a/custom-tasks/custom-task-2.js
+++ packages/project/test/fixtures/application.a/custom-tasks/custom-task-2.js
@@ -4,26 +4,16 @@ module.exports = async function ({
 }) {
 	console.log("Custom task 2 executed");
 
-	// For #1 Build: Read a file which is an input of custom-task-1 (which sets a tag on it)
+	// Read a file which is an input of custom-task-1 (which sets a tag on it):
 	const testJS = await workspace.byPath(`/resources/${projectNamespace}/test.js`);
 
-	// For #3 Build: Read a different file (which is NOT an input of custom-task-1)
-	const test2JS = await workspace.byPath(`/resources/${projectNamespace}/test2.js`);
-
 	const tag = taskUtil.getTag(testJS, taskUtil.STANDARD_TAGS.IsDebugVariant);
-	if (!test2JS && testJS) {
-		// For #1 Build:
-		if (tag) {
-			console.log("Tag set by custom-task-1 is present in custom-task-2, as EXPECTED.");
-		} else {
-			throw new Error("Tag set by custom-task-1 is NOT present in custom-task-2, which is UNEXPECTED.");
-		}
-	} else {
-		// For #3 Build (SAME behavior expected as in #1):
-		if (tag) {
-			console.log("Tag set by custom-task-1 is present in custom-task-2, as EXPECTED.");
-		} else {
-			throw new Error("Tag set by custom-task-1 is NOT present in custom-task-2, which is UNEXPECTED.");
-		}
+	// For #1 & #3 build:
+	if (!tag) {
+		throw new Error("Tag set by custom-task-1 is NOT present in custom-task-2, which is UNEXPECTED.");
 	}
+
+	// For #3 build: Read a different file which is not an input of custom-task-1
+	// (ensures that this task is executed):
+	const test2JS = await workspace.byPath(`/resources/${projectNamespace}/test2.js`);
 };
