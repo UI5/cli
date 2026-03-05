0031e0478c27483e1c79c3af0a13e5dd6b411447
Max Reichmann <max.reichmann@sap.com>
2026-03-05 15:36:16 +0100

    test(project): Address review of @RandomByte

--- packages/project/test/fixtures/application.a/custom-tasks-2/custom-task-1.js
+++ packages/project/test/fixtures/application.a/custom-tasks-2/custom-task-1.js
@@ -1,22 +1,29 @@
+const Logger = require("@ui5/logger");
+const log = Logger.getLogger("builder:tasks:customTask1");
+
 let buildRanOnce;
 module.exports = async function ({
 	workspace, taskUtil,
 	options: {projectNamespace}
 }) {
-	console.log("Custom task 1 executed");
+	log.verbose("Custom task 1 executed");
 
 	// Read a file to trigger execution of this task:
 	const testJS = await workspace.byPath(`/resources/${projectNamespace}/test.js`);
 
 	if (buildRanOnce != true) {
-		console.log("Flag NOT set -> We are in #1 Build still");
+		log.verbose("Flag NOT set -> We are in #1 Build still");
 		buildRanOnce = true;
 		const tag = taskUtil.getTag(testJS, taskUtil.STANDARD_TAGS.IsDebugVariant);
 		if (!tag) {
 			throw new Error("Tag set during #1 Build is not readable, which is UNEXPECTED.");
 		}
 	} else {
-		console.log("Flag set -> We are in #2 Build");
+		const previousTag = taskUtil.getTag(testJS, taskUtil.STANDARD_TAGS.IsDebugVariant);
+		if (previousTag) {
+			throw new Error("Tag set during #1 Build is still readable, which is UNEXPECTED.");
+		}
+		log.verbose("Flag set -> We are in #2 Build");
 		const tag = taskUtil.getTag(testJS, taskUtil.STANDARD_TAGS.OmitFromBuildResult);
 		if (!tag) {
 			throw new Error("Tag set during #2 Build is not readable, which is UNEXPECTED.");
