61ffba8e306c8cc625a1358cf59b682906dfce69
d3xter666 <yavor.ivanov@sap.com>
2026-08-25 14:51:09 +0300

    feat: Add check for manifest.json during library spec initialization

--- packages/project/lib/specifications/types/Library.js
+++ packages/project/lib/specifications/types/Library.js
@@ -194,6 +194,23 @@ class Library extends ComponentProject {
 	async _parseConfiguration(config, buildManifest) {
 		await super._parseConfiguration(config, buildManifest);
 
+		// For Specification Version 5.0+, non-framework libraries must provide a manifest.json
+		if (this.getSpecVersion().gte("5.0") && !this.isFrameworkProject()) {
+			try {
+				await this._getManifest();
+			} catch (err) {
+				throw new Error(
+					`Could not find required manifest.json for library project ${this.getName()}: ` +
+					`${err.message}\n\n` +
+					`Library projects using Specification Version 5.0 or higher must provide ` +
+					`a manifest.json directly in their source directory.\n` +
+					`For migration details, please refer to:\n` +
+					`https://ui5.github.io/cli/updates/migrate-v5/#generatelibrarymanifest-task-no-longer-executed`, {
+						cause: err
+					});
+			}
+		}
+
 		if (buildManifest) {
 			this._namespace = buildManifest.namespace;
 			return;
